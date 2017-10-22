80186cb3fa3b4cf409b38036f0838748b18cafa5
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-22 16:56:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,12 @@ class School
   end
 
   def add_student(student, grade)
-    @roster[grade] = []
-    @roster[grade] << student
+    if @roster[grade] = nil || @roster[grade] = false
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
   end
 
 
