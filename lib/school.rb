753d07b7b2e1102c6687b8a7017a34411d4dd407
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-22 16:23:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,12 @@
 class School
   attr_reader :name
 
-  ROSTER = {}
-
   def initialize(name)
     @name = name
     roster = {}
   end
 
   def add_student(name)
-    ROSTER[name] = 
+    ROSTER[name] =
 
 end
